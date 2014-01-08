01d9ba2e7f451966c5500ac801be19d814477ba4
Tony Edwards <tnydwrds@gmail.com>
2014-01-07 23:43:35 -0800

    Updated setup.py to extend distutils' build_py command.

--- setup.py
+++ setup.py
@@ -25,122 +25,55 @@
 #
 # ***** END GPL LICENCE BLOCK *****
 #
-from argparse import ArgumentParser, RawDescriptionHelpFormatter
-import glob
-import os
-import shutil
-import sys
+from distutils.core import setup
+from distutils.command.build_py import build_py
+import os.path
 import re
+from shutil import make_archive, rmtree
 
-# -----------------------------------------------------------------------------
-# Settings
-# You can changes stuffs here.
-# -----------------------------------------------------------------------------
-dist_directory = 'dist'
-package_directories = ['dson']
-exclude_files = [__file__]
-exclude_patterns = []
-include_files = []
+# Packages, data and additional files.
+packages = ['figuretools', 'figuretools.dson']
 
+class blender_dist(build_py):
+    """ """
 
-# -----------------------------------------------------------------------------
-# Guts
-# You shouldn't change stuffs here. Unless its broken or something.
-# -----------------------------------------------------------------------------
-description = """
-Create a Blender friendly Python distribution.
+    description = \
+    '"build" pure Python modules (copy to build directory) and zip it"
 
-Example:
-  setup.py sdist   wil bundle raw files in a zip suitable for Blender.
+    def run(self):
+        build_dir = self.build_lib = 'tmp_build'
+        super(blender_dist, self).run()
 
-Commands:
-  sdist            create a source distribution (zip file)
-"""
+        archive_name = self.distribution.get_fullname().lower()
+        archive_path = os.path.join('dist', archive_name)
 
-# Convention is that each command in 'available_commands' has a companion
-# module function prefixed with 'do_'. So if there is a command 'sdist', there
-# should be a function named 'do_sdist'.
-#
-# Right now only a source distribution is supported. Perhaps in the future a
-# compiled (.pyc) distribution could be supported.
-available_commands = ['sdist']
+        msg = \
+        'creating \'%s.zip\' and adding \'%s\' (and everything under it) to it'
+        print(msg % (archive_path, root_package))
+        if (not self.dry_run):
+            make_archive(
+                base_name=archive_path,
+                format='zip',
+                root_dir=build_dir,
+                base_dir=root_package)
 
-def do_sdist():
-    """Make source distribution of package"""
-    if not os.path.exists(dist_directory):
-        os.makedirs(dist_directory)
-
-    # Create a clean temporary build directory.
-    package_name = os.path.basename(os.path.split(os.path.realpath(__file__))[0])
-    build_directory = 'build_tmp'
-    working_directory = os.path.join(build_directory, package_name)
-    if os.path.exists(build_directory):
-        shutil.rmtree(build_directory)
-    os.makedirs(working_directory)
-
-    # Running list of distributable files. Starting with modules in current dir.
-    distributable_files = glob.glob('*.py')
-
-    for pd in package_directories:
-        # Add all package modules.
-        py_pathname = os.path.join(pd, '*.py')
-        distributable_files.extend(glob.glob(py_pathname))
-
-        # Create a package directory in our working directory.
-        build_pd = os.path.join(working_directory, pd)
-        if not os.path.exists(build_pd):
-            os.makedirs(build_pd)
-
-    # Remove excluded files from distributable_files.
-    if exclude_patterns:
-        for ep in exclude_patterns:
-            exclude_files.extend(glob.glob(ep))
-    for ef in exclude_files:
-        distributable_files.remove(ef)
-
-    # Add explicitly included files to distributable_files.
-    # TODO: Mabye add include_pattern as well?
-    distributable_files.extend(include_files)
-
-    # Move distributable files to our working directory.
-    for df in distributable_files:
-        df_destination = os.path.join(working_directory, df)
-        shutil.copyfile(df, df_destination)
-
-    # Get addon version number.
-    version_re = '["\']version["\']\s*?:\s*?\(\s*?(\d+?)\s*?,\s*?(\d+?)\s*?,\s*?(\d+?)\s*?\)'
-    version = re.search(version_re, open('__init__.py', 'r').read())
-    version = '.'.join(version.groups()) if version else '0.0.0'
-
-    # Make the zip file.
-    zip_file_name = '%s-%s' % (package_name, version)
-    shutil.make_archive(
-        base_name=os.path.join(dist_directory, zip_file_name),
-        format='zip',
-        root_dir=build_directory,
-        base_dir=package_name)
-
-    # Remember to clean up the build directory.
-    shutil.rmtree(build_directory)
+        print('removing %s (and everything under it)' % build_dir)
+        if (not self.dry_run):
+            rmtree(build_dir)
 
 
-def main(args):
-    parser = ArgumentParser(
-        formatter_class=RawDescriptionHelpFormatter,
-        description=description)
+# For now, assume addon has one root package and it is the first package.
+root_package = packages[0]
 
-    parser.add_argument('cmd', help='See available commands above')
+# Reuse addon details from bl_info in the root package.
+bl_info_re = re.compile('bl_info\s*?=\s*?({.*?})', re.DOTALL)
+bl_info_file = os.path.join(root_package, '__init__.py')
+bl_info_match = bl_info_re.search(open(bl_info_file, 'r').read())
+bl_info = eval(bl_info_match.groups()[0])
 
-    args = parser.parse_args(args)
-
-    # Perform command.
-    cmd = args.cmd
-    if cmd in available_commands:
-        # Using globals feels like the wrong way to do this, but it works.
-        globals()['do_' + cmd]()
-    else:
-        print("error: invalid command '%s'" % cmd)
-
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
+setup(
+    name=bl_info['name'],
+    version='%s.%s.%s' % bl_info['version'],
+    author=bl_info['author'],
+    packages=packages,
+    cmdclass={'blender_dist': blender_dist})
